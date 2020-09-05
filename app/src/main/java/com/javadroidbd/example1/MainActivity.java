a9ba37237f3ec9650c767fbfe726a39c20584c99
resilientbd <faisal.hossain.pk@gmail.com>
2020-09-05 20:38:51 +0600

    get,putextra till

--- app/src/main/java/com/javadroidbd/example1/MainActivity.java
+++ app/src/main/java/com/javadroidbd/example1/MainActivity.java
@@ -44,9 +44,6 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
-
-
-
     }
 //    public boolean formvalidation()
 //    {
--- app/src/main/java/com/javadroidbd/example1/SecondActivity.java
+++ app/src/main/java/com/javadroidbd/example1/SecondActivity.java
@@ -2,13 +2,20 @@ package com.javadroidbd.example1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView emailtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        emailtext=findViewById(R.id.emailtext);
+        emailtext.setText(""+getIntent().getStringExtra("email"));
+
     }
 }
